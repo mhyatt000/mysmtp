e16f7dd266cd13fb218d4a9099f576aab244a272
Matt Hyatt <mhyatt000@gmail.com>
2025-12-10 18:04:41 -0500

    integrate  rocketry

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from dotenv import load_dotenv
 from mysmtp.email import Mailer
+from mysmtp.subproc import do, parse, lines
 
-def main():
-    load_dotenv()
+from rocketry import Rocketry
+from rocketry.conds import daily
+
+load_dotenv()
+app = Rocketry()
+
+# @app.task(daily)
+@app.task(daily.after("07:00"))
+def do_daily():
     M = Mailer()
     M.send(subject="Test Email", message="Hello, this is a test email from Python.")
 
-if __name__ == "__main__":
+# @app.task(cron("* 2 * * *"))
+# def do_based_on_cron():
+
+def main():
+    do_daily()
+    app.run()
+
+if __name__ == '__main__':
     main()
